a410e65c3d4ae85f81c289abdcfb40a9aeb10f29
Marvel <mc.faithful@stu.unizik.edu.ng>
2024-12-17 20:35:15 +0100

    omo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ["@radix-ui"],
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
